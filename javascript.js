a5be6054d80841d5b908b0884b6d642ae005ba14
Arqam Waheed <86839084+ArqamWaheed@users.noreply.github.com>
2025-02-04 13:14:46 +0500

    MY best friends birthday 🥺

---
+++ javascript.js
@@ -0,0 +1,32 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
+}
+
+const heartsContainer = document.querySelector('.hearts');
+
+function createHeart() {
+    const heart = document.createElement('span');
+    heart.style.left = Math.random() * 100 + 'vw';
+    heart.style.animationDuration = Math.random() * 3 + 2 + 's';
+    heartsContainer.appendChild(heart);
+
+    setTimeout(() => {
+        heart.remove();
+    }, 5000);
+}
+
+setInterval(createHeart, 300);
